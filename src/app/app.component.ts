79f2140ce277258c227e0bf17b9bfb5dda20408b
aminjonshermatov <aminjonshermatov@gmail.com>
2021-11-01 19:54:12 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  public title = 'frontend';
+  public counter = 0;
+
+
+  ngOnInit(): void {
+    this.title = 'Awesome app';
+  }
+
+  public increase(): void {
+    this.counter++;
+  }
+
+  public decrease(): void {
+    this.counter--;
+  }
 }
