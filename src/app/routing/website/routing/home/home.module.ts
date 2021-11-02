e17424a31c35f8a040d05d51628082c8767184dc
aminjonshermatov <aminjonshermatov@gmail.com>
2021-11-02 15:02:13 +0300

    added angular material

--- src/app/routing/website/routing/home/home.module.ts
+++ src/app/routing/website/routing/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import {RouterModule} from "@angular/router";
+import {MatSliderModule} from "@angular/material/slider";
 
 
 
@@ -11,6 +12,7 @@ import {RouterModule} from "@angular/router";
   ],
   imports: [
     CommonModule,
+    MatSliderModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: HomePageComponent }
     ])
