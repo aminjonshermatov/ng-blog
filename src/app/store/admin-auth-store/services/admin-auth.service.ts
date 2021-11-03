7b7ebfd2a34ca73b18e232b6c531872c2b47dba8
aminjonshermatov <aminjonshermatov@gmail.com>
2021-11-03 17:46:20 +0300

    overrided interceptor to rxjs

--- src/app/store/admin-auth-store/services/admin-auth.service.ts
+++ src/app/store/admin-auth-store/services/admin-auth.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
 import {AuthData} from "../store/admin-auth.reducer";
 import {JwtHelperService} from "@auth0/angular-jwt";
-import {select, Store} from "@ngrx/store";
-import {getAccessToken} from "../store/admin-auth.selectors";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminAuthService {
-  public accessToken = '';
-
   constructor(
     private readonly httpClient: HttpClient,
-    private readonly jwtHelperService: JwtHelperService,
-    private readonly store$: Store
-  ) {
-    this.store$.pipe(select(getAccessToken))
-      .subscribe(accessToken => this.accessToken = accessToken || '');
-  }
+    private readonly jwtHelperService: JwtHelperService
+  ) { }
 
   public login(body: { login: string, password: string }) {
     return this.httpClient.post<AuthData>(
