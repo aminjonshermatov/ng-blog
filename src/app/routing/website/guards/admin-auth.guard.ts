388f6a53fbf01b3e32c6bed1023bf987650ed84a
aminjonshermatov <aminjonshermatov@gmail.com>
2021-11-04 11:30:26 +0300

    connected guards to store

--- src/app/routing/website/guards/admin-auth.guard.ts
+++ src/app/routing/website/guards/admin-auth.guard.ts
@@ -1,17 +1,46 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree
+} from '@angular/router';
+import {Observable} from 'rxjs';
+import {first, map} from "rxjs/operators";
+import {AdminAuthService} from "../../../store/admin-auth-store/services/admin-auth.service";
 
 @Injectable()
 export class AdminAuthGuard implements CanActivate, CanLoad {
+  constructor(
+    private readonly router: Router,
+    private readonly adminAuthService: AdminAuthService
+  ) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this._getIsAuth();
   }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this._getIsAuth();
+  }
+
+  private _getIsAuth(): Observable<boolean> {
+    return this.adminAuthService.isAuth$.pipe(
+      first(),
+      map(isAuth => {
+        if (!isAuth) {
+          this.router.navigateByUrl('/admin/auth/login');
+        }
+
+        return isAuth;
+      })
+    );
   }
 }
--- src/app/routing/website/guards/admin-guest.guard.ts
+++ src/app/routing/website/guards/admin-guest.guard.ts
@@ -1,18 +1,47 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import {AdminAuthService} from "../../../store/admin-auth-store/services/admin-auth.service";
+import {first, map} from "rxjs/operators";
 
 @Injectable()
 export class AdminGuestGuard implements CanActivate, CanLoad {
+  constructor(
+    private readonly router: Router,
+    private readonly adminAuthService: AdminAuthService
+  ) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this._getIsGuest();
   }
 
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this._getIsGuest();
+  }
+
+  private _getIsGuest(): Observable<boolean> {
+    return this.adminAuthService.isGuest$.pipe(
+      first(),
+      map(isGuest => {
+        if (!isGuest) {
+          this.router.navigateByUrl('/admin');
+        }
+
+        return isGuest;
+      })
+    );
   }
 }
